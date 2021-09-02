f50f24f95e16b70d03a409e3910c8428092cdf68
dreamfalldown <1787833352@qq.com>
2021-09-02 18:49:17 +0800

    feat: db connect

--- main/util/connect.test.ts
+++ main/util/connect.test.ts
@@ -1,7 +1,36 @@
-import DBConnection from './connect';
+import DBConnection, { ConnectArgs } from './connect';
+import * as M from 'mysql2/node_modules/iconv-lite';
+import { error } from 'console';
+// solve encode error
+M.encodingExists('foo');
 
 describe('DBConnection', () => {
-    it('1', () => {
-        console.log(123);
+    let db:DBConnection | null = null;
+    it('The database can be connected correctly', () => {
+        const args: ConnectArgs = {
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            dialect: 'mysql'
+        };
+        db = new DBConnection(args);
+        expect(db).toBeDefined();
+    });
+
+    it('The database authenticate correctly', async () => {
+        expect.assertions(1);
+
+        if (!db) return;
+        const connected = await db.authenticate();
+        expect(connected).toBe(true);
+    });
+
+    it('can get all databases', async () => {
+        expect.assertions(1);
+
+        if (!db) return;
+        const res = await db.getAllDatabase();
+        expect(res).toBeDefined();
     });
 });
--- main/util/connect.ts
+++ main/util/connect.ts
@@ -1,10 +1,10 @@
-import { Sequelize } from 'sequelize';
-
+import { Dialect, Sequelize, QueryTypes } from 'sequelize';
 export interface ConnectArgs {
     host: string;
     port: number;
     username: string;
     password: string;
+    dialect: Dialect;
 }
 class DBConnection {
     public sequelize: Sequelize;
@@ -15,17 +15,26 @@ class DBConnection {
     connect(args: ConnectArgs): Sequelize {
     	const sequelize = new Sequelize(args);
 
-    	sequelize
-    		.authenticate()
-    		.catch(error =>
-    			console.error('Unable to connect to the database:', error)
-    		);
-
     	return sequelize;
     }
 
     switchConnect(args: ConnectArgs) {
     	this.sequelize = this.connect(args);
+    }
+
+    async authenticate(): Promise<boolean> {
+        try {
+            await this.sequelize.authenticate();
+            return true;
+        } catch(error) {
+            console.error('Unable to connect to the database:', error);
+            return false;
+        }
+    }
+
+    async getAllDatabase() {
+        const dbs: { Database: string }[] = await this.sequelize.query('show databases', { type: QueryTypes.SELECT });
+        return dbs.map(db => db.Database);
     }
 }
 
